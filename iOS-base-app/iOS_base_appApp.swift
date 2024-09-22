9d1174fd54f3cbb91092a37e4e98444c3f35c877
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-09-22 23:07:39 +0900

    Initial Commit

---
+++ iOS-base-app/iOS_base_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_base_appApp.swift
+//  iOS-base-app
+//
+//  Created by 이영호 on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_base_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
